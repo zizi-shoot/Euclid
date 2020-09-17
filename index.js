b2309f07cee5cd26c851e5db6f831e7ed2249e17
Полянский Олег <ol.polyanskiy@gmail.com>
2020-09-17 21:21:06 +0300

    fix burger

--- index.js
+++ index.js
@@ -27,19 +27,8 @@ const burgerBtn = document.querySelector('.burger');
 const burgerMenu = document.querySelector('.burger-nav');
 
 document.querySelector('.header__burger').addEventListener('click', () => {
-	const line1 = document.querySelector('.header__burger:hover .line-1');
-	const line2 = document.querySelector('.line-2');
-	const line3 = document.querySelector('.header__burger:hover .line-3');
-
-	if (line2.style.transform === '') {
-		line1.style.transform = 'translate(0px, 10px) rotate(45deg)';
-		line2.style.transform = 'translateX(100px)';
-		line3.style.transform = 'translate(0px, -10px) rotate(-45deg)';
-	} else {
-		line1.style.transform = '';
-		line2.style.transform = '';
-		line3.style.transform = '';
-	}
+	const burgerLine = document.querySelectorAll('.burger__line');
+	burgerLine.forEach((el) => el.classList.toggle(`${el.classList[1]}--active`));
 });
 
 burgerBtn.addEventListener('click', () => {
